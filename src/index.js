412df09996d860155ff7f5a92881d2f8a5c86345
Samad Ariyan <samadariyan@yahoo.com>
2024-05-10 03:17:26 +0330

    updated to es6 modules

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import uniqueRandomCourse from "unique-random-array";
 import courses from "./courses.json";
 
-export const all = courses;
-export const random = uniqueRandomCourse(courses);
+const getRandomItem = uniqueRandomArray(starWarsNames);
+
+module.exports = {
+  all: starWarsNames,
+  random: random,
+};
+
+function random(number) {
+  if (number === undefined) {
+    return getRandomItem();
+  } else {
+    const randomItems = [];
+    for (let i = 0; i < number; i++) {
+      randomItems.push(getRandomItem());
+    }
+    return randomItems;
+  }
+}
